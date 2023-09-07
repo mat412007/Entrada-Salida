38b374b7a26fa068287c01f44cc63de73d9cc2c5
Appaz <matias2007play4@gmail.com>
2023-09-07 12:53:01 -0300

    Comienzo

--- ejercicio3.c
+++ ejercicio3.c
@@ -2,14 +2,14 @@
 #include <stdlib.h>
 
 int main(int argc, char *argv[]){
+    int n;
     int num;
-    int sum = 0;
-    do{
+    int suma = 0;
+    scanf("%d", &n);
+    for(int i = 0; i < n; i++){
         scanf("%d", &num);
-        sum += num;
+        suma += num;
     }
-    while(num != 0);
-    printf("%d\n", sum);
+    printf("%d\n", suma);
     return 0;
-    
 }
